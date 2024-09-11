2fcc1500b9a18e0be22522b92edb24a7d01f2d54
Promaveus <mavroskamnde@gmail.com>
2024-09-11 09:47:27 -0600

    Lab 2

---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default StudentInfo(){
+    return(
+        <main>
+            <p>Mavros Andrews</p>
+            <Link href=
+        </main>
+    );
+
+}
